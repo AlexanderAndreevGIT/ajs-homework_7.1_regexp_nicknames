be612319ecea72817820524b6a14f6f092c9e0ef
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2021-11-11 00:36:42 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ export default class Validator {
   static validateUsername(username) {
     const ifMistake = [
       /[^A-Za-z0-9_-]/.test(username), // проверка на на наличие других символов кроме латинских букв, -, _ и цифр (0-9) (если есть, значение true)
-      /\d{3}/.test(username), // проверка на на наличие 3х цифр подряд (если есть, значение true)
+      /\d{3}/.test(username), // проверка на наличие 3х цифр подряд (если есть, значение true)
       /^(\d|_|-)/.test(username), // проверка на цифру, -, _ в начале (если есть, значение true)
       /(\d|_|-)$/.test(username), // проверка на цифру, -, _ в конце (если есть, значение true)
     ];
